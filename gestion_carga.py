87aff8f1ed91341d1c0e7130a0d7a4ba8cf033d8
Jinx <jinxynet@duck.com>
2025-12-20 19:38:48 +0100

    first commit

---
+++ gestion_carga.py
@@ -0,0 +1,15 @@
+# Constantes de color
+ROJO = "\033[31m"  # Para errores
+VERDE = "\033[32m"  # Para éxitos
+AMARILLO = "\033[33m"  # Para información
+AZUL = "\033[34m"  # Para títulos
+RESET = "\033[0m"  # Para volver al color por defecto
+
+# Valores iniciales
+carga = [
+    {"nombre": "Combustible", "peso": 2000, "tipo": "Propulsión"},
+    {"nombre": "Comida", "peso": 500, "tipo": "Consumible"},
+    {"nombre": "Oxígeno", "peso": 1000, "tipo": "Vital"},
+]
+
+PESO_MAXIMO = 5000
